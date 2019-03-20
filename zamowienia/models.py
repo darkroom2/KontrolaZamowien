6b8a53200468dfa60f1ec811ec8dd55f5c2bee2c
darkroom2 <283680@pw.edu.pl>
2019-03-20 22:39:55 +0100

    v1.0

--- zamowienia/models.py
+++ zamowienia/models.py
@@ -8,7 +8,7 @@ class Pracownik(models.Model):
     imie = models.CharField(max_length=20)
     nazwisko = models.CharField(max_length=20)
 
-    def __unicode__(self):
+    def __str__(self):
         return '{} {}'.format(self.imie, self.nazwisko)
 
 
@@ -16,14 +16,14 @@ class Wyplata(models.Model):
     pracownik = models.ForeignKey(Pracownik, on_delete=models.PROTECT)  # TODO: pole Stanowisko (i w Pracownik)
     kwota = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
-    def __unicode__(self):
-        return '{}zł\t{}'.format(self.kwota, self.pracownik)
+    def __str__(self):
+        return '{}zł___{}'.format(self.kwota, self.pracownik)
 
 
 class Sprzet(models.Model):
     nazwa = models.CharField(max_length=200)
 
-    def __unicode__(self):
+    def __str__(self):
         return '{}'.format(self.nazwa)
 
 
@@ -32,8 +32,8 @@ class Cena(models.Model):
     cena = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     data_wprowadzenia_ceny = models.DateTimeField('obowiązuje od', auto_now_add=True)
 
-    def __unicode__(self):
-        return 'od: {}\t{}zł\t{}'.format(self.data_wprowadzenia_ceny, self.cena, self.sprzet)
+    def __str__(self):
+        return 'od:_{}___{}zł___{}'.format(self.data_wprowadzenia_ceny.date(), self.cena, self.sprzet)
 
 
 class Pozycja(models.Model):
@@ -42,8 +42,8 @@ class Pozycja(models.Model):
     poczatek_uzywania = models.DateTimeField('początek używania')
     koniec_uzywania = models.DateTimeField('koniec używania')
 
-    def __unicode__(self):
-        return 'od: {}\tdo: {}\t{}\t{}'.format(self.poczatek_uzywania, self.koniec_uzywania, self.ilosc, self.sprzet)
+    def __str__(self):
+        return 'od:_{}___do:_{}___{}___{}'.format(self.poczatek_uzywania.date(), self.koniec_uzywania.date(), self.ilosc, self.sprzet)
 
 
 class Zamowienie(models.Model):
@@ -51,16 +51,16 @@ class Zamowienie(models.Model):
     czy_nowe = models.BooleanField(default=True)
     data_zlozenia = models.DateTimeField('złożono', auto_now_add=True)
 
-    def __unicode__(self):
-        return '{}\tnowe: {}\t{}'.format(self.data_zlozenia, self.czy_nowe, self.pracownik_skladajacy)
+    def __str__(self):
+        return '{}___{}___nowe: {}___{}'.format(self.id, self.data_zlozenia.date(), self.czy_nowe, self.pracownik_skladajacy)
 
 
 class ListaPozycji(models.Model):
     zamowienie = models.ForeignKey(Zamowienie, on_delete=models.PROTECT)
     pozycja = models.ForeignKey(Pozycja, on_delete=models.PROTECT)
 
-    def __unicode__(self):
-        return '{}\t{}'.format(self.zamowienie, self.pozycja)
+    def __str__(self):
+        return '{}___{}'.format(self.zamowienie, self.pozycja)
 
 
 class ListaObslugujacych(models.Model):
@@ -68,5 +68,5 @@ class ListaObslugujacych(models.Model):
     pracownik = models.ForeignKey(Pracownik, on_delete=models.PROTECT)
     data_przypisania = models.DateTimeField('przypisano', auto_now_add=True)
 
-    def __unicode__(self):
-        return '{}\t{}\t{}'.format(self.data_przypisania, self.zamowienie, self.pracownik)
+    def __str__(self):
+        return '{}___{}___{}'.format(self.data_przypisania.date(), self.zamowienie, self.pracownik)
